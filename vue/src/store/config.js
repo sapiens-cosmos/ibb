bbb607cbc1f24dfb167edbbbd9c47dfab0521636
delivan <wjdgurdl272@gmail.com>
2021-06-21 18:45:42 +0900

    Remove front-end generated module

--- vue/src/store/config.js
+++ vue/src/store/config.js
@@ -1,9 +1,10 @@
 import { env, blocks, wallet, transfers, relayers } from '@starport/vuex'
-import generated from './generated'
+// import generated from './generated'
 export default function init(store) {
+	/*
 	for (const moduleInit of Object.values(generated)) {
 		moduleInit(store)
-	}
+	}*/
 	transfers(store)
 	blocks(store)
 	env(store)
