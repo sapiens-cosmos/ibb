919727ae33cd58db1981be318e080cf3d43bb8c8
Jihoon Song <jihoonsong.dev@gmail.com>
2021-07-01 23:03:34 +0900

    Append new TxHistory to the user when withdraw occurs

--- x/ibb/keeper/msg_server_withdraw.go
+++ x/ibb/keeper/msg_server_withdraw.go
@@ -31,6 +31,15 @@ func (k msgServer) CreateWithdraw(goCtx context.Context, msg *types.MsgCreateWit
 			queryUser.Deposit[i].Amount = queryUser.Deposit[i].Amount - msg.Amount
 		}
 	}
+
+	var txHistory types.TxHistory
+	txHistory.BlockHeight = int32(ctx.BlockHeight())
+	txHistory.Tx = "withdraw"
+	txHistory.Asset = msg.Asset
+	txHistory.Amount = msg.Amount
+	txHistory.Denom = msg.Denom
+	queryUser.TxHistories = append(queryUser.TxHistories, &txHistory)
+
 	k.SetUser(ctx, queryUser)
 
 	creatorAddress, err := sdk.AccAddressFromBech32(msg.Creator)
